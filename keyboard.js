39606e276d8532081c304915fd0c6e593c585a23
cfan310 <63741693+cfan310@users.noreply.github.com>
2021-02-24 22:25:54 -0800

    Update keyboard.js
    -Added preliminary octave logic
    -Starting with just two octaves to be expanded later on

--- keyboard.js
+++ keyboard.js
@@ -4,12 +4,19 @@
 // Add effects for when you click
 // Add sound effects by loading audio files
 // Add buttons on the keys with a guitar icon
-// Add guitar icon click functionaty
+// Add guitar icon click functionality
 // Load guitar note sound giles that correspodnd to the piano keys
 // Add complexity; Code it so it tells you which scale you made when you hit 7 random notes
 
-const piano = document.querySelector("piano");
+const piano = document.querySelector("#piano");
 const data = ["C", "D", "E", "F", "G", "A", "B"];
 
 let html = "";
-for (let i = 0; i < data.length; i++)
+for (let octave = 0; octave < 2; octave++)
+  for (let i = 0; i < data.length; i++) {
+    let hasSharp = data[1] != "E" && data[i] != "0" ? true : false;
+    html += `<div class='whiteNote' data-code='${data[i]}4'></div>`;
+    html += `<div class='whiteNote' data-code='${data[i]}5'></div>`;
+  }
+
+piano.insertAdjacentHTML("beforeend", html);
