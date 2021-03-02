a3191347f9e1121b6daab4d3725473a0f70d0798
cfan310 <63741693+cfan310@users.noreply.github.com>
2021-03-01 17:19:09 -0800

    Update keyboard.js
    -Storing black and white key notes in a variable
    -Troubleshooting black keys problem; They are not correctly positioned in between the major keys

--- keyboard.js
+++ keyboard.js
@@ -13,21 +13,18 @@ const data = ["C", "D", "E", "F", "G", "A", "B"];
 
 let html = "";
 // loop over first two octaves; CDEFGAB -> CDEFGAB
-for (let octave = 0; octave < 2; octave++)
+for (let octave = 0; octave < 2; octave++) {
   for (let i = 0; i < data.length; i++) {
     let hasSharp = data[i] != "E" && data[i] != "B" ? true : false;
-    html += `<div class='whiteNote' data-code='${data[i]}${octave + 4}'></div>`; // C4 D4 E4....
-   
-    }
-  }
-
-piano.insertAdjacentHTML("beforeend", html);
-
-/*
-if (hasSharp) {
-      html += `<div class='whiteNote' data-code='${data[i]}#${
+    html += `<div class='whiteNote' data-code='${data[i]}${octave + 4}'>`;
+    if (hasSharp) {
+      html += `<div class='blackNote' data-code='${data[i]}#${
         octave + 4
       }'></div>`;
-      html += "</div>";
     }
-*/
+    html += `</div>`;
+  }
+}
+piano.insertAdjacentHTML("beforeend", html);
+
+const notes = document.querySelectorAll(".whiteNote", "blackNote");
